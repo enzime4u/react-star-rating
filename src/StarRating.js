aa1ec83d146ebe4430303e1646c5e8a48a47b82c
Liviu Tudor <enzime4u@yahoo.com>
2021-04-14 17:37:31 +0300

    extract onSelect fn

--- src/StarRating.js
+++ src/StarRating.js
@@ -5,14 +5,14 @@ const createArray = (length) => [...Array(length)];
 
 export default function StarRating({ totalStars = 5 }, ...props) {
   const [selectedStars, setSelectedStars] = useState(0);
-
+  const onSelect = (n) => setSelectedStars(n + 1);
   return (
-    <div className="wrapper" style={{ padding: 10 }} {...props}>
+    <div className="wrapper" {...props}>
       {createArray(totalStars).map((n, i) => (
         <Star
           key={i}
           selected={selectedStars > i}
-          onSelect={() => setSelectedStars(i + 1)}
+          onSelect={() => onSelect(i)}
         />
       ))}
       <p className="text">
